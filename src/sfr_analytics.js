09fbeca02a03eae56f4611db928fd6d368e173be
Md Saddam <mdsonu7860@outlook.com>
2025-12-31 12:42:26 +0530

    feat: implement daily delta sync from SFR Analytics to HubSpot

--- src/sfr_analytics.js
+++ src/sfr_analytics.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-// import "./jobs/scheduler.job.js"; // This will automatically start the scheduler
+import "./jobs/scheduler.job.js"; // This will automatically start the scheduler
 import { app, logger, sfrcAnalyticsTohubspotSync } from "./index.js";
 
 const PORT = process.env.PORT || 5000;
